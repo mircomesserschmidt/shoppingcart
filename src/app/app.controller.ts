726dac219fc3c3cc57586a27ef78f7dbdef59f9c
Mirco Messerschmidt <75323003+mircomesserschmidt@users.noreply.github.com>
2022-02-14 10:26:17 +0100

    change amount of shopping cart/ checkout cart

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Put, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 import CreateShoppingCartDto from '../dtos/CreateShoppingCartDto';
 import InsertProductDto from '../dtos/InsertProductDto';
@@ -6,18 +16,18 @@ import { AppService } from './app.service';
 
 @Controller('cart')
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Put()
   @HttpCode(200)
   async createShoppingCart(@Body() body: CreateShoppingCartDto) {
     try {
-      const result = await this.appService.createShoppingCart(body.userId)
+      const result = await this.appService.createShoppingCart(body.userId);
       return {
-        "cartId": result
-      }
+        cartId: result,
+      };
     } catch (error) {
-      return error
+      return error;
     }
   }
 
@@ -25,41 +35,43 @@ export class AppController {
   async insertIntoShoppinCart(
     @Param('cartId') cartId: number,
     @Body() body: InsertProductDto,
-    @Res() response: Response
+    @Res() response: Response,
   ) {
     try {
-      await this.appService.insertIntoCart(cartId, body.productId, body.amount)
-      const contents = await this.appService.getContentsOfCart(cartId)
-      response.status(200).send(
-        {
-          "cart": contents
-        }
-      )
+      await this.appService.insertIntoCart(cartId, body.productId, body.amount);
+      const contents = await this.appService.getContentsOfCart(cartId);
+      response.status(200).send({
+        cart: contents,
+      });
     } catch (error) {
-      response.status(HttpStatus.NOT_FOUND).send(error)
+      response.status(HttpStatus.NOT_FOUND).send(error);
     }
   }
 
-  @Put()
-  changeAmountInShoppingCart() {
-
+  @Put(':cartid/product/:productid/amount')
+  async changeAmountInShoppingCart(
+    @Param('cartId') cartId: number,
+    @Param('productid') productId: number,
+    @Body() amount: number,
+  ) {
+    return await this.appService.changeAmountOfProduct(
+      cartId,
+      productId,
+      amount,
+    )
   }
 
   @Delete(':cartid/product/:productid')
   @HttpCode(200)
-  deleteFromShoppinCart(
+  async deleteFromShoppinCart(
     @Param('cartid') cartId: number,
     @Param('productid') productId: number,
   ) {
-    return this.appService.removeFromCart(cartId, productId)
+    return await this.appService.removeFromCart(cartId, productId);
   }
-
 
   @Get(':cartid/submit')
-  checkoutShoppingCart(
-    @Param('cartid') cartId: number
-  ) {
-
+  async checkoutShoppingCart(@Param('cartid') cartId: number) {
+    return await this.appService.getContentsOfCart(cartId);
   }
-
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,108 +2,110 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Contents } from '../entities/Contents';
 import { ShoppingCart } from '../entities/ShoppingCart';
-import { Repository } from "typeorm";
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
-
   constructor(
     @InjectRepository(ShoppingCart)
     private shoppingCartRepo: Repository<ShoppingCart>,
 
     @InjectRepository(Contents)
-    private contentsRepo: Repository<Contents>
-  ) {
-
-  }
+    private contentsRepo: Repository<Contents>,
+  ) {}
 
   async createShoppingCart(userId: number): Promise<number> {
-    const shoppingcart = new ShoppingCart(userId)
+    const shoppingcart = new ShoppingCart(userId);
     const result = await this.shoppingCartRepo.find({
-      where: { user: userId }
-    })
+      where: { user: userId },
+    });
 
     if (result && result.length > 0) {
-      throw { "error": `Shoppingcart with user id <${userId}> already exists.` }
-    }
-    else {
-      await this.shoppingCartRepo.insert(shoppingcart)
-      return shoppingcart.id
+      throw { error: `Shoppingcart with user id <${userId}> already exists.` };
+    } else {
+      await this.shoppingCartRepo.insert(shoppingcart);
+      return shoppingcart.id;
     }
   }
 
   async insertIntoCart(cartId: number, productId: number, amount: number) {
     if (await this.doesCartExists(cartId)) {
-      const content = new Contents(cartId, productId, amount)
-      return await this.contentsRepo.insert(content)
-    }
-    else {
+      const content = new Contents(cartId, productId, amount);
+      return await this.contentsRepo.insert(content);
+    } else {
       throw {
-        "error": `Shopping cart with id  <${cartId}> not found.`
-      }
+        error: `Shopping cart with id  <${cartId}> not found.`,
+      };
     }
   }
 
   async removeFromCart(cartId: number, productId: number) {
-    if (await this.doesCartExists(cartId) && await this.isProductInCart(cartId, productId)) {
-      const res = await this.contentsRepo.findOne({ where: { shoppingCartId: cartId, productid: productId } })
+    if (
+      (await this.doesCartExists(cartId)) &&
+      (await this.isProductInCart(cartId, productId))
+    ) {
+      const res = await this.contentsRepo.findOne({
+        where: { shoppingCartId: cartId, productid: productId },
+      });
       if (res) {
-        this.contentsRepo.remove(res)
-        return res
-      }
-      else{
+        this.contentsRepo.remove(res);
+        return res;
+      } else {
         throw {
-          "error": `Product with id <${productId}> could not be removed form cart with id <${cartId}>.`
-        }
+          error: `Product with id <${productId}> could not be removed form cart with id <${cartId}>.`,
+        };
       }
-    }
-    else {
+    } else {
       throw {
-        "error": `Product with id <${productId}> could not be removed form cart with id <${cartId}>.`
-      }
+        error: `Product with id <${productId}> could not be removed form cart with id <${cartId}>.`,
+      };
     }
   }
-
 
   async getContentsOfCart(cartId: number) {
     return await this.contentsRepo.find({
-      select: ["productid", "quantity"],
-      where: { shoppingCartId: cartId }
-    })
+      select: ['productid', 'quantity'],
+      where: { shoppingCartId: cartId },
+    });
   }
 
-  async changeAmountOfProduct(cartId: number, productId: number, amount: number) {
+  async changeAmountOfProduct(
+    cartId: number,
+    productId: number,
+    amount: number,
+  ): Promise<Boolean> {
     const res = await this.contentsRepo.findOne({
       where: {
-        shoppingCartId: 1,
-        productid: "productId"
-      }
-    })
+        shoppingCartId: cartId,
+        productid: productId,
+      },
+    });
 
     if (res) {
-      res.quantity = amount
-      this.contentsRepo.save(res)
-    }
-    else {
-      console.log("Error changing amount of product");
+      res.quantity = amount;
+      this.contentsRepo.save(res);
+      return true;
+    } else {
+      console.log('Error changing amount of product');
+      return false;
     }
   }
-
-
 
   private async doesCartExists(cartId: number): Promise<boolean> {
-    const res = await this.shoppingCartRepo.findOne(cartId)
-    return res !== null && res !== undefined
+    const res = await this.shoppingCartRepo.findOne(cartId);
+    return res !== null && res !== undefined;
   }
 
-  private async isProductInCart(cartId: number, productId: number): Promise<boolean> {
+  private async isProductInCart(
+    cartId: number,
+    productId: number,
+  ): Promise<boolean> {
     const res = await this.contentsRepo.findOne({
       where: {
-        shoppingCartId: cartId, productid: productId
-      }
-    })
-    return res !== null && res !== undefined
+        shoppingCartId: cartId,
+        productid: productId,
+      },
+    });
+    return res !== null && res !== undefined;
   }
-
-
 }
